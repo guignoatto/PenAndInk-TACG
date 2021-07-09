05e2f641acbc6c77c1342e938aee31561a26d3a1
Guilherme Gnoatto <guifgnoatto@gmail.com>
2021-07-09 18:52:51 -0300

    .

---
+++ Trabalho GC - TACG/Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [Range(0, 1)]
+    [SerializeField] private float speed;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.down * speed);
+    }
+}
